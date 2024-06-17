f752caff3df13cb9d4fe8ce06e6b32670204f6bf
Yash <yashsatyawali70@gmail.com>
2024-06-17 20:58:53 +0530

    update.

--- src/Components/common/Footer.jsx
+++ src/Components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made by Himanshu Sangwan 🗿 © 2023 Studynotion</div>
+          <div className="text-center">Made by YASH SHARMA 🗿 © 2024 Studynotion</div>
         </div>
       </div>
     </div>
